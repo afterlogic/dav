1bef1598149f7ad2b5f4fdc2578f5af357a9be22
sash04ek <a.ovcharov@gmail.com>
2016-12-07 14:53:11 +0300

    fix db prefix

--- lib/DAV/CardDAV/Backend/PDO.php
+++ lib/DAV/CardDAV/Backend/PDO.php
@@ -14,8 +14,7 @@ class PDO extends \Sabre\CardDAV\Backend\PDO {
     public function __construct() {
 
 		parent::__construct(\CApi::GetPDO());
-		
-		$sDbPrefix = \CApi::GetSettings()->GetConf('Common/DBPrefix');
+		$sDbPrefix = \CApi::GetSettings()->GetConf('DBPrefix');
 		$this->cardsTableName = $sDbPrefix.Constants::T_CARDS;
 		$this->addressBooksTableName = $sDbPrefix.Constants::T_ADDRESSBOOKS;
 		$this->addressBookChangesTableName = $sDbPrefix.Constants::T_ADDRESSBOOKCHANGES;
