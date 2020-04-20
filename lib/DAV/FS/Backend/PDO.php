f4b8e6ca26eebaa9779dcd6893db1baa41dd237b
sash04ek <a.ovcharov@gmail.com>
2020-04-20 13:26:32 +0300

    fixed - BUG - if data folder is not writable the API responds with 500 error

--- lib/DAV/FS/Backend/PDO.php
+++ lib/DAV/FS/Backend/PDO.php
@@ -18,34 +18,38 @@ class PDO
 	 * @var string
 	 */
 	protected $dBPrefix;
-	
+
 	/**
 	 * @var string
 	 */
 	protected $pdo;
-	
+
 	/**
 	 * @var string
 	 */
-	protected $sharedFilesTableName;	
+	protected $sharedFilesTableName;
 
 	/**
 	 * Creates the backend
 	 */
-	public function __construct() 
+	public function __construct()
 	{
 		$this->pdo = \Aurora\System\Api::GetPDO();
-		$this->dBPrefix = \Aurora\System\Api::GetSettings()->DBPrefix;
+		$oSettings = \Aurora\System\Api::GetSettings();
+		if ($oSettings)
+		{
+			$this->dBPrefix = \Aurora\System\Api::GetSettings()->DBPrefix;
+		}
 		$this->sharedFilesTableName = $this->dBPrefix.'adav_sharedfiles';
 	}
-	
+
     /* @param string $principalUri
      * @return array
      */
     public function getSharedFilesForUser($principalUri) {
 
 		$aResult = [];
-		
+
 		$fields[] = 'id';
         $fields[] = 'owner';
         $fields[] = 'principaluri';
@@ -64,7 +68,7 @@ SQL
         );
 
 		$stmt->execute([$principalUri]);
-		while($row = $stmt->fetch(\PDO::FETCH_ASSOC))		
+		while($row = $stmt->fetch(\PDO::FETCH_ASSOC))
 		{
 			$aResult[] = [
 				'id' => $row['id'],
@@ -77,10 +81,10 @@ SQL
 				'isdir' => (bool) $row['isdir'],
 			];
 		}
-		
+
 		return $aResult;
 	}
-	
+
     /* @param string $principalUri
     /* @param string $path
      * @return array
@@ -88,7 +92,7 @@ SQL
     public function getSharedFile($principalUri, $path) {
 
 		$aResult = false;
-		
+
 		$fields[] = 'id';
         $fields[] = 'owner';
         $fields[] = 'principaluri';
@@ -108,7 +112,7 @@ SQL
 
 		$stmt->execute([$principalUri, $path]);
         $row = $stmt->fetch(\PDO::FETCH_ASSOC);
-		
+
 		if ($row)
 		{
 			$aResult = [
@@ -122,9 +126,9 @@ SQL
 				'isdir' => (bool) $row['isdir'],
 			];
 		}
-		
+
 		return $aResult;
-	}	
+	}
 
     /* @param string $principalUri
     /* @param string $uid
@@ -133,7 +137,7 @@ SQL
     public function getSharedFileByUid($principalUri, $uid) {
 
 		$aResult = false;
-		
+
 		$fields[] = 'id';
         $fields[] = 'owner';
         $fields[] = 'principaluri';
@@ -153,7 +157,7 @@ SQL
 
 		$stmt->execute([$principalUri, $uid]);
         $row = $stmt->fetch(\PDO::FETCH_ASSOC);
-		
+
 		if ($row)
 		{
 			$aResult = [
@@ -167,9 +171,9 @@ SQL
 				'isdir' => (bool) $row['isdir'],
 			];
 		}
-		
+
 		return $aResult;
-	}		
+	}
 
 	/* @param string $owner
 	 * @param string $storage
@@ -179,7 +183,7 @@ SQL
     public function getShares($owner, $storage, $path) {
 
 		$aResult = [];
-		
+
 		$fields[] = 'id';
         $fields[] = 'owner';
         $fields[] = 'storage';
@@ -196,19 +200,19 @@ SQL
         );
 
 		$stmt->execute([$owner, $storage, $path]);
-		while($row = $stmt->fetch(\PDO::FETCH_ASSOC))		
+		while($row = $stmt->fetch(\PDO::FETCH_ASSOC))
 		{
 			$aResult[] = [
 				'principaluri' => $row['principaluri'],
 				'access' => (int) $row['access'],
 			];
 		}
-		
+
 		return $aResult;
-	}	
-	
+	}
+
 	/**
-	 * 
+	 *
 	 * @param string $owner
 	 * @param string $storage
 	 * @param string $path
@@ -245,15 +249,15 @@ SQL
 		$stmt = $this->pdo->prepare("INSERT INTO ".$this->sharedFilesTableName." (".implode(', ', $fieldNames).") VALUES (".implode(', ',array_keys($values)).")");
         $stmt->execute($values);
 
-        return $this->pdo->lastInsertId();			
+        return $this->pdo->lastInsertId();
 	}
 
 	public function updateSharedFileName($principalUri, $uid, $name)
 	{
 		$stmt = $this->pdo->prepare('UPDATE ' . $this->sharedFilesTableName . ' SET `uid` = ? WHERE principaluri = ? AND uid = ?');
 		return  $stmt->execute([$name, $principalUri, $uid]);
-	}	
-	
+	}
+
 	public function updateShare($owner, $storage, $path, $newPath)
 	{
 		$stmt = $this->pdo->prepare(
@@ -262,10 +266,10 @@ SQL
 			WHERE path LIKE ? AND owner = ? AND storage = ?"
 		);
 		return  $stmt->execute([$path, $newPath, $path . '%', $owner, $storage]);
-	}	
+	}
 
 	/**
-	 * 
+	 *
 	 * @param string $owner
 	 * @param string $storage
 	 * @param string $path
@@ -274,11 +278,11 @@ SQL
 	public function deleteSharedFile($owner, $storage, $path)
 	{
         $stmt = $this->pdo->prepare('DELETE FROM '.$this->sharedFilesTableName.' WHERE owner = ? AND storage = ? AND path = ?');
-        return $stmt->execute([$owner, $storage, $path]);		
+        return $stmt->execute([$owner, $storage, $path]);
 	}
 
 	/**
-	 * 
+	 *
 	 * @param string $owner
 	 * @param string $path
 	 * @return bool
@@ -286,17 +290,17 @@ SQL
 	public function deleteShare($principaluri, $uid)
 	{
         $stmt = $this->pdo->prepare('DELETE FROM '.$this->sharedFilesTableName.' WHERE principaluri = ? AND uid = ?');
-        return $stmt->execute([$principaluri, $uid]);		
-	}	
-	
+        return $stmt->execute([$principaluri, $uid]);
+	}
+
 	/**
-	 * 
+	 *
 	 * @param string $principalUri
 	 * @return bool
 	 */
 	public function deleteSharesByPrincipal($principalUri)
 	{
         $stmt = $this->pdo->prepare('DELETE FROM '.$this->sharedFilesTableName.' WHERE principaluri = ? OR owner = ?');
-        return $stmt->execute([$principalUri, $principalUri]);		
-	}		
+        return $stmt->execute([$principalUri, $principalUri]);
+	}
 }
