1744bb33ccaea48db5a210b82e49643331bc3903
sash04ek <a.ovcharov@afterlogic.com>
2022-01-11 17:05:05 +0300

    fixed BUG - PHP Notice when attempt to create Corporate storage

--- lib/DAV/FS/Local/Corporate/Root.php
+++ lib/DAV/FS/Local/Corporate/Root.php
@@ -18,7 +18,15 @@ class Root extends Directory
 
 	public function __construct() 
 	{
-		$path = \Aurora\System\Api::DataPath() . \Afterlogic\DAV\Constants::FILESTORAGE_PATH_ROOT . \Afterlogic\DAV\Constants::FILESTORAGE_PATH_CORPORATE;
+		$path = \Aurora\System\Api::DataPath() . \Afterlogic\DAV\Constants::FILESTORAGE_PATH_ROOT;
+		
+		if (!\file_exists($path))
+		{
+			\mkdir($path);
+		}
+
+		$path = $path . \Afterlogic\DAV\Constants::FILESTORAGE_PATH_CORPORATE;
+
 		if (!file_exists($path))
 		{
 			\mkdir($path);
