1c2884268654968a784442a5d49566e0815a6aa2
sash04ek <a.ovcharov@afterlogic.com>
2023-01-17 12:10:28 +0300

    Add check for Calendar module availability in Dav

---
+++ lib/DAV/Permission.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ */
+
+namespace Afterlogic\DAV;
+
+/**
+ * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
+ * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
+ * @copyright Copyright (c) 2019, Afterlogic Corp.
+ */
+class Permission
+{
+    public const Write = 1;
+    public const Read = 2;
+    public const Reshare = 3;
+}
